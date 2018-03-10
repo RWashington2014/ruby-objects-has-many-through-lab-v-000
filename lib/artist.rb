df5c72ca2a4118d91e501400d0f6b89579365029
Robert W <robwash@gmail.com>
2018-03-10 00:33:38 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,5 +6,21 @@ class Artist
       @songs = []
     end
 
+    def songs
+      @songs
+    end
+
+    def add_song(s_obj)
+      @songs << s_obj
+      s_obj.artist = self
+    end
+
+    def genres
+      @songs.collect do |song|
+        song.genre
+      end
+
+    end
+
 
 end # => End Artist
