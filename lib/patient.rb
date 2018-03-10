8bbf42817553dbedc54a458e6dfe7edbe090d7bf
Robert W <robwash@gmail.com>
2018-03-10 00:53:01 +0000

    automatically backed up by learn

---
+++ lib/patient.rb
@@ -0,0 +1,24 @@
+class Patient
+  attr_accessor :name
+
+    def initialize(name)
+      @name = name
+      @appointments = []
+    end
+
+    def appointments
+      @appointments
+    end
+
+    def add_appointment(ap_obj)
+      self.appointments << ap_obj
+      ap_obj.paitent = self
+    end
+
+    def doctors
+      self.appointments.collect do |app|
+        app.doctors
+      end
+
+    end
+end
